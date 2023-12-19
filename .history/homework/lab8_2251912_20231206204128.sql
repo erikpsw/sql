e055f47ba6c016210078015b9d8745eebc1bd8cd
Shiwei Pan <1923437477@qq.com>
2023-12-19 13:54:00 +0800

    commit

--- .history/homework/lab8_2251912_20231206204128.sql
+++
@@ -1,9 +0,0 @@
--- create view 学生成绩
--- as 
--- select sc.snum,sections.cnum,course.cname,sc.score
--- from sc,sections,course
--- where sc.secnum=sections.secnum and sections.cnum=course.cnum
-
-SELECT * FROM 学生成绩
-
-CREATE PROCEDURE Proc
